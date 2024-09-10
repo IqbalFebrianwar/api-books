68f7c0dcf0991ca56b4740542fc8c1f030d6d005
IqbalFebrianwar <zonelyrics26@gmail.com>
2024-09-10 16:19:21 +0700

    Feat : Configurate 5

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -18,16 +9,14 @@ const loan_route_1 = __importDefault(require("./routes/loan.route"));
 const book_route_1 = __importDefault(require("./routes/book.route"));
 const app = (0, express_1.default)();
 const PORT = 3000;
-const main = () => __awaiter(void 0, void 0, void 0, function* () {
-    app.use(express_1.default.json());
-    app.use('/', user_route_1.default);
-    app.use('/', book_route_1.default);
-    app.use('/', loan_route_1.default);
-    app.get('/', (req, res) => {
-        res.json({ message: "Hello world!" });
-    });
-    app.listen(PORT, () => {
-        console.log(`server is running in PORT ${PORT}`);
-    });
+app.use(express_1.default.json());
+app.use('/', user_route_1.default);
+app.use('/', book_route_1.default);
+app.use('/', loan_route_1.default);
+app.get('/', (req, res) => {
+    res.json({ message: "Hello world!" });
 });
-main();
+app.listen(PORT, () => {
+    console.log(`server is running in PORT ${PORT}`);
+});
+module.exports = app;
