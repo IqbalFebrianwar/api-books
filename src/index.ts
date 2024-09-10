e9793d04d782c3abc910931f9f05377ab74f0553
IqbalFebrianwar <zonelyrics26@gmail.com>
2024-09-10 15:15:07 +0700

    Feat : Rebuild main

--- src/index.ts
+++ src/index.ts
@@ -6,15 +6,18 @@ import bookRoutes from './routes/book.route';
 const app = express();
 const PORT = 3000;
 
-app.use(express.json());
-app.use('/', userRoutes);
-app.use('/', bookRoutes);
-app.use('/', loanRoutes);
-
-app.get('/', (req, res) => {
-    res.json({ message: "Hello world!" });
-});
-
-app.listen(PORT, ()=> {
-    console.log(`server is running in PORT ${PORT}`)
-})
+const main = async () => {
+    app.use(express.json());
+    app.use('/', userRoutes);
+    app.use('/', bookRoutes);
+    app.use('/', loanRoutes);
+    
+    app.get('/', (req, res) => {
+        res.json({ message: "Hello world!" });
+    });
+    
+    app.listen(PORT, ()=> {
+        console.log(`server is running in PORT ${PORT}`)
+    })
+}
+main()
